c462a93bbd49585501761ceeff3e02af7ec30f9e
Folaranmi <107549891+Jokike@users.noreply.github.com>
2022-06-19 16:18:51 +0100

    Add files via upload

---
+++ assets/subway.js
@@ -0,0 +1,25 @@
+
+
+//  passenger subway solution
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el")
+let count = 0;
+
+
+function increment() {
+    count+=1  // INSTEAD OF THIS count = count + 1 D0:
+    //instead of  countEl.innerText = count  DO:
+    countEl.textContent = count
+}
+
+function save(){
+                // Create a variable that contains both the count and the dash separator, i.e. "12 - "
+                let countStr = count + " - "
+                // 3. Render the variable in the saveEl using innerText
+                 saveEl.textContent += countStr //instead of  saveEl.innerText += countStr 
+                 countEl.textContent = 0
+                 count =0
+                 console.log(count)
+}
+
+
